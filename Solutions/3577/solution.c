b2820032ca8a90e86f892bcadfe008d04bb53749
gbentham06 <gbentham06@gmail.com>
2025-12-10 10:15:07 +0000

    3577

---
+++ Solutions/3577/solution.c
@@ -0,0 +1,16 @@
+#define MOD 1000000007
+
+int countPermutations(int* complexity, int complexitySize) {
+    int first = complexity[0];
+
+    for (int i = 1; i < complexitySize; i++) {
+        if (complexity[i] <= first) return 0;
+    }
+
+    long long fact = 1;
+    for (int i = 2; i < complexitySize; i++) {
+        fact = (fact * i) % MOD;
+    }
+
+    return (int)fact;
+}
